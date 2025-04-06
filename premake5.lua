876a0ccda9a0cf6110c7d183ac1037a897d2f79f
WenzhiShao <643636057@qq.com>
2025-04-06 16:55:03 +0800

    接入glfw窗口按键事件，修改工程配置

--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,17 @@
 project "Imgui"
     kind "StaticLib"
     language "C++"
- 
+
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("intermidiate/" .. outputdir .. "/%{prj.name}")
- 
+
+    includedirs
+    {
+        ".",
+        "./backends",
+        "../GLFW/include",
+    }
+
     files
     {
         "imconfig.h",
@@ -18,12 +25,16 @@ project "Imgui"
         "imstb_textedit.h",
         "imstb_truetype.h",
         "imgui_draw.cpp",
+        "./backends/imgui_impl_glfw.h",
+        "./backends/imgui_impl_glfw.cpp",
+        "./backends/imgui_impl_opengl3.h",
+        "./backends/imgui_impl_opengl3.cpp"
     }
-    
+
     filter "system:windows"
         systemversion "latest"
         cppdialect "C++17"
         staticruntime "On"
-        
+
     filter { "system:windows", "configurations:Release" }
         buildoptions "/MT"
